362589cab2dae7e268e14b627fb08e19287d65d7
BernabasGirma <bernabasgirma00@gmail.com>
2022-10-17 12:01:43 +0300

    updated

--- print_custom.c
+++ print_custom.c
@@ -97,12 +97,12 @@ int print_rot13(va_list l, flags_t *f)
 /**
  * print_percent - prints a percent
  * @l: va_list arguments from _printf
- * @f: pointer to the struct flags in which we turn the flags on
+OAOAOA * @f: pointer to the struct flags in which we turn the flags on
  * Return: number of char printed
- */
+OAOAOA */
 int print_percent(va_list l, flags_t *f)
 {
 	(void)l;
-	(void)f;
+OAOAOA	(void)f;
 	return (_putchar('%'));
 }
